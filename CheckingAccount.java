2f8cc53c7569abd14956c6c935cc85d634dc4648
Matahhhhh <93481766+Matahhhhh@users.noreply.github.com>
2022-03-15 11:50:38 +0800

    Update CheckingAccount.java

--- CheckingAccount.java
+++ CheckingAccount.java
@@ -2,14 +2,22 @@ public class CheckingAccount {
     private double balance;
     private String account_no;
 
-    public void deposit(double amount){
+    public void deposit(double amount) throws IllegalArgumentException{
+        if (amount < 0){
+            throw new IllegalArgumentException("Deposit value has to be a positive number");
+        }
+        else{
         this.balance += amount;
+        }
     }
 
 
-    public void withdraw(double amount) throws InsufficientFundsException{
+    public void withdraw(double amount) throws InsufficientFundsException, IllegalArgumentException{
         if (amount > balance){
             throw new InsufficientFundsException(amount);
+        }
+        else if (amount < 0){
+            throw new IllegalArgumentException("Withdraw value has to be a positive number");
         }
         else {
             this.balance -= amount;
